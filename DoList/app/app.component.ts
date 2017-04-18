dc6a009f0f3e8eab2f83588b14c098bc68bf35c7
Tonghann Teng <tengtonghann@gmail.com>
2017-04-17 23:46:22 -0500

    commit

---
+++ DoList/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  items = ["Angular 4", "React", "Java"]
+
+  newItem = "";
+
+  pushItem = function () {
+
+    if (this.newItem != ""){
+      this.items.push(this.newItem);
+      this.newItem = "";
+    }
+  }
+
+  removeItem = function (index) {
+    this.items.splice(index, 1);
+  }
+}
